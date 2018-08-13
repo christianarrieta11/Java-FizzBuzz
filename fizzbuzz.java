239d5ab17ec4cfbeeeeab6069d419150c3ced3b3
Christian Arrieta <christianarrieta11@gmail.com>
2018-08-13 15:06:30 -0700

    Add files via upload

---
+++ fizzbuzz.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class fizzbuzz{
+
+         public static void main(String args []){
+         
+         
+         
+         for (int i = 1; i <= 100; i++) {
+         
+         if(i % 3 == 0 && 5 ==0){
+            
+            System.out.println("fizzBuzz" );
+            
+            }
+              else if(i % 3 == 0){
+                   System.out.println("fizz");
+              }
+              
+                     else if(i % 5 == 0){
+                  System.out.println("Buzz");
+               }
+               }
+
+         
+         
+         }
+         
+         
+         }
+         
+
+         
+         
+            
+           
+             
+              
+
+          
+            
+
+
+          
+          
+         
+       
+
+         
+
+
+       
